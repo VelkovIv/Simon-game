26616ab23095b7da33b9c9fd2860b82c68ed2d1b
VelkovIv <ivaylo.velkov@gmail.com>
2023-01-08 20:59:31 +0200

    finale touch

--- game.js
+++ game.js
@@ -8,13 +8,13 @@ $(".btn").click(function() {
     userClickedPatern.push(userChosenColour);
     playSound(userChosenColour);
     animatedPress(userChosenColour);
-    checkAnswer(userChosenColour);
+    checkAnswer(userClickedPatern.length-1);
     // console.log(userClickedPatern);
 });
 
 $(document).keypress(function() {
     if (level === 0){
-        $("#level-title").text("Level " + level);
+        // $("#level-title").text("Level " + level);
         nextSequence();
     }
 });
@@ -24,12 +24,20 @@ function checkAnswer(currentLevel) {
         console.log("success");
         if (userClickedPatern.length === gamePattern.length) {
             setTimeout(function() {
-                nextSequence()
-            },1000)
+                nextSequence();
+            },1000);
         }
 
     } else {
         console.log("wrong");
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over")
+        },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
 }
 
@@ -43,8 +51,13 @@ function nextSequence() {
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
     
     playSound(randomChosenColour);
-    $("#level-title").text("Level " + level);
     level++;
+    $("#level-title").text("Level " + level);
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
 }
 
 function playSound(name) {
@@ -54,7 +67,7 @@ function playSound(name) {
 
 function animatedPress(currentColour) {
     var pressedButton = $("#" + currentColour);
-    pressedButton.addClass("pressed")
+    pressedButton.addClass("pressed");
 
     setTimeout(function() {
         pressedButton.removeClass("pressed");
